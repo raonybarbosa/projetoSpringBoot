679613eee74d16d0b4edc6598d0cae64ee1a851f
Raony Hudson da Hora Barbosa <raonyhudson@gmail.com>
2019-12-19 16:18:14 -0300

    Ajustes no manipulador de exception

--- src/main/java/br/com/ecommerce/exception/advice/AdviceExceptionHandler.java
+++ src/main/java/br/com/ecommerce/exception/advice/AdviceExceptionHandler.java
@@ -1,8 +1,8 @@
 package br.com.ecommerce.exception.advice;
 
-import br.com.bl2.ecommerce.exception.*;
 import br.com.ecommerce.exception.*;
 import br.com.ecommerce.response.Error;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,49 +27,43 @@ public class AdviceExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity(new Error(HttpStatus.BAD_REQUEST.toString(),
                 "Requisição possui campos inválidos",
                 ex.getBindingResult().getFieldErrors().stream()
-                        .map(error -> error.getDefaultMessage())
+                        .map(DefaultMessageSourceResolvable::getDefaultMessage)
                         .collect(Collectors.toList())), status);
     }
 
     /*@ExceptionHandler(CustomException.class)
     ResponseEntity naoEncontradaHandler(CustomException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Categoria não encontrada.",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }*/
 
     @ExceptionHandler(CategoriaNaoEncontradaException.class)
     ResponseEntity categoriaNaoEncontradaHandler(CategoriaNaoEncontradaException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Categoria não encontrada.",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DescontoNaoEncontradoException.class)
     ResponseEntity descontoNaoEncontradoHandler(DescontoNaoEncontradoException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Desconto não encontrado",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(FornecedorNaoEncontradoException.class)
     ResponseEntity fornecedorNaoEncontradoHandler(FornecedorNaoEncontradoException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Fornecedor não encontrado",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UnidadeNaoEncontradaException.class)
     ResponseEntity unidadeNaoEncontradoHandler(UnidadeNaoEncontradaException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Unidade não encontrado",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ProdutoNaoEncontradoException.class)
     ResponseEntity produtoNaoEncontradoHandler(ProdutoNaoEncontradoException ex) {
         return new ResponseEntity(new Error(HttpStatus.NOT_FOUND.toString(), "Produto não encontrado",
-                mountDescription(ex)), HttpStatus.NOT_FOUND);
-    }
-
-    private List<String> mountDescription(Exception ex) {
-        description = new ArrayList<>();
-        description.add(ex.getMessage());
-        return description;
+                Arrays.asList(ex.getMessage())), HttpStatus.NOT_FOUND);
     }
 }
